27794c3ea2c549a9bd8709189d72cd778f9bee19
Eric Chen <chen.ericsn@outlook.com>
2023-12-02 16:03:42 -0600

    tested and fixed bugs with the step parser

--- utils/substep.py
+++ utils/substep.py
@@ -5,11 +5,11 @@ from ontologies import *
 from typing import Dict
 from ingredient import RecipeIngredient
 from spacy.matcher import Matcher
-from step import RecipeStep
+from spacy.matcher import PhraseMatcher
 
 nlp = spacy.load('en_core_web_sm')
 
-class RecipeSubstep(RecipeStep):
+class RecipeSubstep():
     def __init__(self, doc, recipe_ingredients, actions, parent_step_number):
         self.text = doc.text
         self.parent_step_number = parent_step_number
@@ -22,6 +22,11 @@ class RecipeSubstep(RecipeStep):
         self.tools = self.get_tools(doc)
         self.duration, self.end_condition = self.get_duration(doc)
         self.temperature = self.get_temperature(doc)
+        
+        self.additional_info = None
+    
+    def add_additional_info(self, info):
+        self.additional_info = info
     
     def get_temperature(self, doc: spacy.tokens.doc.Doc):
         temperature = []
@@ -43,6 +48,10 @@ class RecipeSubstep(RecipeStep):
                             temp = doc[start-1].text + " heat"
                             if start > 2 and doc[start-2].text == "to" and doc[start-3].text == "low" or doc[start-3].text == "medium" or doc[start-3].text == "high":
                                 temp = doc[start-3:end].text
+                        
+                        for action in reversed(self.secondary_actions):
+                            if "heat" in action[0]:
+                                self.secondary_actions.remove(action)
                     
                     elif temp_word == "degrees":
                         if start > 0 and doc[start-1].pos_ == "NUM":
@@ -51,19 +60,19 @@ class RecipeSubstep(RecipeStep):
                         temp = temp_word
                     
                     if temp: 
-                        temperature.append(temp)
+                        temperature.append((temp,start))
         
         return temperature
     
     def get_tools(self, doc: spacy.tokens.doc.Doc):
         tools = []
-        matcher = Matcher(nlp.vocab)
+        matcher = PhraseMatcher(nlp.vocab)
         patterns = [nlp.make_doc(tool) for tool in COOKING_TOOLS]
         matcher.add("tools", patterns)
         
         for match_id, start, end in matcher(doc):
             span = doc[start:end]
-            tools.append(span.text)
+            tools.append((span.text,start))
         
         return tools
     
@@ -72,12 +81,13 @@ class RecipeSubstep(RecipeStep):
         end_condition = []
         for ent in doc.ents:
             if ent.label_ == "TIME":
-                duration.append(ent.text)
+                duration.append((ent.text,ent.start))
     
         # Handle the "until" keyword
+        doc = nlp(doc.text.split(',')[0])
         matcher = Matcher(nlp.vocab)
         pattern = [{"LOWER": 'until'}]
-        matcher.add("until", pattern)
+        matcher.add("until", [pattern])
         
         for match_id, start, end in matcher(doc):
             end_cond = ''
@@ -85,19 +95,19 @@ class RecipeSubstep(RecipeStep):
                 head = doc[start].head
                 for item in head.subtree:
                     end_cond += item.text + ' '
-                end_condition.append(end_cond.strip())
+                end_condition.append((end_cond.strip(),start))
                 
             elif doc[start].dep_ == "prep":
                 for item in doc[start].children:
                     end_cond += item.text + ' '
-                end_condition.append(end_cond.strip())
+                end_condition.append((end_cond.strip(),start))
                 
         return duration, end_condition
     
     def get_ingredients(self, doc: spacy.tokens.doc.Doc, ingredients: Dict[str, RecipeIngredient]):
         # String match the ingredients
         patterns = [nlp.make_doc(ing) for ing in ingredients.keys()]
-        matcher = Matcher(nlp.vocab)
+        matcher = PhraseMatcher(nlp.vocab)
         matcher.add("ingredients", patterns)
         matches = matcher(doc)
         
@@ -106,13 +116,13 @@ class RecipeSubstep(RecipeStep):
         for match_id, start, end in matches:
             span = doc[start:end]
             ingredients[span.text].add_step(self.parent_step_number)
-            step_ingredients.append(ingredients[span.text])
+            step_ingredients.append((ingredients[span.text],start))
             
         # do a second pass
         for token in doc: 
-            for ing in step_ingredients.keys():
+            for ing in ingredients.keys():
                 if token.text in ing:
-                    step_ingredients.append(ingredients[ing])
+                    step_ingredients.append((ingredients[ing],token.i))
                     ingredients[ing].add_step(self.parent_step_number)
                     
         return step_ingredients
@@ -125,7 +135,7 @@ class RecipeSubstep(RecipeStep):
         misc_actions = []
         
         # match the cooking actions
-        matcher = Matcher(nlp.vocab)
+        matcher = PhraseMatcher(nlp.vocab)
         primary_patterns = [nlp.make_doc(method) for method in PRIMARY_COOKING_ACTIONS]
         matcher.add("primary_methods", primary_patterns)
         secondary_patterns = [nlp.make_doc(method) for method in SECONDARY_COOKING_ACTIONS]
@@ -134,14 +144,17 @@ class RecipeSubstep(RecipeStep):
         for match_id, start, end in matcher(doc):
             span = doc[start:end]
             if nlp.vocab.strings[match_id] == "primary_methods":
-                primary_actions.append(span.text)
+                primary_actions.append((span.text,start))
             elif nlp.vocab.strings[match_id] == "secondary_methods":
-                secondary_actions.append(span.text)
+                secondary_actions.append((span.text,start))
         
         # find the misc actions
         if len(primary_actions) == 0 and len(secondary_actions) == 0:
             for token in doc:
                 if token.dep_ == "ROOT" and token.pos_ == "VERB" and token.pos_ == "VB":
-                    misc_actions.append(token.text)
+                    misc_actions.append((token.text, token.i))
                     
         return primary_actions, secondary_actions, misc_actions
+    
+
+    
